e272a9631e2133473c8e68dfb974e8a547933eb4
IMRANDIL <aliimranadil2@gmail.com>
2022-04-07 19:19:35 +0530

    yh

--- server.js
+++ server.js
@@ -40,14 +40,27 @@ con.connect(function (err) {
     console.log('Database connected😄');
     // const sql = 'SELECT COUNT(*) AS total FROM users';
     // const sql = `INSERT INTO users (email) VALUES ("whey@gmail.com")`;
-    const person = {
-        email: faker.internet.email(),
-        created_at: faker.date.past()
-    };
+
+    //Inserting Lots of data.........500....
+
+    // const person = {
+    //     email: faker.internet.email(),
+    //     created_at: faker.date.past()
+    // };
 
 
-    const sql = `INSERT INTO users SET ?`;     //insert data dynamically....
-    con.query(sql, person, function (err, result, fields) {
+    const data = [];
+
+    for (let i = 0; i < 500; i++) {
+        data.push([faker.internet.email(), faker.date.past()])
+    }
+
+    console.log(data.length);
+
+
+    // const sql = `INSERT INTO users SET ?`;     //insert data dynamically....
+    const sql = `INSERT INTO users (email,created_at) VALUES ?`;
+    con.query(sql, [data], function (err, result, fields) {
         if (err) throw err;
         console.log(result);
 
